978271127df61bd788dbcc773ed27d16bf0225dc
Levin Herr <github@17d.me>
2022-04-13 18:32:17 +0200

    add error handling w/ notyf

--- src/main.js
+++ src/main.js
@@ -17,6 +17,38 @@ Vue.prototype.notyf = new Notyf({
   dismissible: true,
 });
 
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      store.dispatch("logout");
+      Vue.prototype.notyf.error("Logged out!");
+      return Promise.reject(error);
+    }
+    if (error.response.status === 406) {
+      if (router.currentRoute.name !== "privacy") {
+        //we do not need any "privacy policy not accepted" errors on the privacy policy
+        router.push({
+          name: "privacy",
+          query: {
+            validFrom: error.response.meta.validFrom,
+            acceptedAt: error.response.meta.acceptedAt,
+          },
+        });
+        return Promise.reject(error);
+      }
+    }
+    if (error.response.errors.length > 0) {
+      error.response.errors.forEach((error) => {
+        Vue.prototype.notyf.error(error);
+      });
+    } else {
+      // Vue.prototype.notyf.error(this.i18n.get("_.messages.exception.general"));
+    }
+    return Promise.reject(error);
+  }
+);
+
 new Vue({
   router,
   store,
@@ -34,36 +66,6 @@ new Vue({
     if (token) {
       this.$store.commit("setUserToken", JSON.parse(token));
     }
-    axios.interceptors.response.use(
-      (response) => response,
-      (error) => {
-        if (error.response.status === 401) {
-          this.$store.dispatch("logout");
-          return Promise.reject(error);
-        }
-        if (error.response.status === 406) {
-          if (router.currentRoute.name !== "privacy") {
-            //we do not need any "privacy policy not accepted" errors on the privacy policy
-            router.push({
-              name: "privacy",
-              query: {
-                validFrom: error.response.meta.validFrom,
-                acceptedAt: error.response.meta.acceptedAt,
-              },
-            });
-            return Promise.reject(error);
-          }
-        }
-        if (error.response.errors.length > 0) {
-          error.response.errors.forEach((error) => {
-            Vue.prototype.notyf.error(error);
-          });
-        } else {
-          // Vue.prototype.notyf.error(this.i18n.get("_.messages.exception.general"));
-        }
-        return Promise.reject(error);
-      }
-    );
   },
   vuetify,
   render: (h) => h(App),
