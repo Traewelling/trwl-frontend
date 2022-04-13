00253cd1df909816059df61c0df708ca50da58d8
Levin Herr <github@17d.me>
2022-04-13 16:27:51 +0200

    base functionality I guess?

--- src/main.js
+++ src/main.js
@@ -3,11 +3,30 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
   router,
   store,
+  created() {
+    const userInfo = localStorage.getItem("user");
+
+    if (userInfo) {
+      this.$store.commit("setUser", JSON.parse(userInfo));
+    }
+    axios.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          if (error.response.status === 401) {
+            this.$store.dispatch("logout");
+          }
+          return Promise.reject(error);
+        }
+    );
+  },
   render: (h) => h(App),
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,50 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+//ToDo: Set this into the config file
+axios.defaults.baseURL = "http://localhost:8000/api/v1";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    user: null,
+    token: null,
+  },
+
+  mutations: {
+    setUserData(state, userData) {
+      state.user = userData;
+      localStorage.setItem("user", JSON.stringify(userData));
+    },
+    setUserToken(state, token) {
+      state.token = token;
+      axios.defaults.headers.common.Authorization = `Bearer ${token.token}`;
+      localStorage.setItem("token", JSON.stringify(state.token));
+    },
+    clearUserData() {
+      localStorage.removeItem("user");
+      location.reload();
+    },
+  },
+  actions: {
+    login({ commit }, credentials) {
+      return axios.post("/auth/login", credentials).then(({ data }) => {
+        commit("setUserToken", data.data);
+        this.dispatch("fetchUserData");
+      });
+    },
+    fetchUserData({ commit }) {
+      axios.get("/auth/user").then(({ data }) => {
+        commit("setUserData", data.data);
+      });
+    },
+    logout({ commit }) {
+      commit("clearUserData");
+    },
+  },
+  getters: {
+    isLogged: (state) => !!state.user,
+  },
 });
