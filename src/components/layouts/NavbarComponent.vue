091ca98081dcd4144815f727211ad380596d27fc
Levin herr <github@17d.me>
2022-04-19 17:34:54 +0200

    toolbar w/ links, fixed on top

--- src/components/layouts/NavbarComponent.vue
+++ src/components/layouts/NavbarComponent.vue
@@ -1,13 +1,50 @@
 <template>
-  <v-app-bar color="#C72730" dense dark>
+  <v-toolbar color="#C72730" dense dark class="fixed-bar">
     <v-app-bar-nav-icon
       class="navbar-toggler d-md-none"
       :aria-label="$i18n.get('_.Toggle navigation')"
       @click="$emit('offCanvas')"
     />
-    <router-link tag="v-toolbar-title" :to="{ name: 'base' }">
+    <router-link tag="v-toolbar-title" :to="{ name: 'index' }" class="me-4">
       {{ $appName }}
     </router-link>
+    <v-toolbar-items>
+      <v-btn
+        v-if="$store.state.authenticated"
+        text
+        :to="{ name: 'dashboard' }"
+        v-text="$i18n.get('_.menu.dashboard')"
+      />
+      <v-btn
+        v-if="$store.state.authenticated"
+        text
+        :to="{ name: 'leaderboard' }"
+        v-text="$i18n.get('_.menu.leaderboard')"
+      />
+      <v-btn
+        text
+        :to="{ name: 'statuses.active' }"
+        v-text="$i18n.get('_.menu.active')"
+      />
+      <v-btn
+        v-if="$store.state.authenticated"
+        text
+        :to="{ name: 'statistics' }"
+        v-text="$i18n.get('_.stats')"
+      />
+      <v-btn
+        v-if="!$store.state.authenticated"
+        text
+        :to="{ name: 'about' }"
+        v-text="$i18n.get('_.menu.about')"
+      />
+      <v-btn
+        v-if="!$store.state.authenticated"
+        text
+        :to="{ name: 'blog' }"
+        v-text="$i18n.get('_.menu.blog')"
+      />
+    </v-toolbar-items>
     <v-spacer />
 
     <v-btn icon>
@@ -23,7 +60,6 @@
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
 
-
     <v-menu offset-y>
       <template v-slot:activator="{ on, attrs }">
         <v-btn icon v-bind="attrs" v-on="on">
@@ -31,14 +67,12 @@
         </v-btn>
       </template>
       <v-list>
-        <v-list-item
-          v-for="n in 5" :key="n"
-        >
+        <v-list-item v-for="n in 5" :key="n">
           <v-list-item-title>{{ n }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
-  </v-app-bar>
+  </v-toolbar>
 </template>
 
 <script>
@@ -47,7 +81,7 @@ import Notifications from "@/ApiClient/Notifications";
 
 export default {
   name: "NavbarComponent",
-  components: {  NotificationsButton },
+  components: { NotificationsButton },
   data() {
     return {
       notificationsCount: 0,
@@ -75,4 +109,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.fixed-bar {
+  position: sticky;
+  position: -webkit-sticky; /* for Safari */
+  top: 0em;
+  z-index: 999;
+}
+</style>
