4e10653b85d683c21fc86d4f7357108366817ffc
Levin Herr <github@17d.me>
2022-07-17 19:10:38 +0200

    replace favicons + change manifest

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Träwelling",
+    themeColor: "c72730",
+    msTileColor: "c72730",
+    appleMobileWebAppCapable: "yes",
+    assetsVersion: "",
+  },
 };
